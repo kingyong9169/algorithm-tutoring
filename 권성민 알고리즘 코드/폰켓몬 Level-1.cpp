d343f12665d5a05a761aee636e5c1f0c8ca3657f
kwonja <snna58@naver.com>
2021-11-19 17:36:06 +0900

    [PG]폰켓몬Lv-1

--- 권성민 알고리즘 코드/폰켓몬 Level-1.cpp
+++ 권성민 알고리즘 코드/폰켓몬 Level-1.cpp
@@ -3,22 +3,12 @@ using namespace std;
 vector<int> select;
 int solution(vector<int> nums)
 {
-	int check = 0;
-	int choose = (nums.size() / 2); //포켓몬을 뽑는 개수
+	int check = 0; int choose = (nums.size() / 2); //포켓몬을 뽑는 개수
 	for (int i = 0; i < nums.size(); i++)
 	{
 		check = 0;
-		for (int j = 0; j < select.size(); j++)
-		{
-			if (nums[i] == select[j])
-			{
-				check = 1;
-			}
-		}
-		if (check == 0)
-		{
-			select.push_back(nums[i]); //처음꺼 선택
-		}
+		for (int j = 0; j < select.size(); j++)if (nums[i] == select[j])check = 1;
+		if (check == 0)select.push_back(nums[i]); //처음꺼 선택
 	}
 	if (select.size() <= choose)return select.size();
 	else return choose;
